a75d6be615b5ee58e8857fc4dc65477c1db35a10
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-09-18 10:12:35 -0600

    middles complete

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,7 @@
 module Arrays
-	
+	def middles array_1, array_2
+		[array_1[1], array_2[3]]
+	end
 	
 	
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -10,5 +10,10 @@ class ArraysTest < MiniTest::Test
      include Arrays
    	end.new
 	end
-
+	def middles
+		assert_equal [1, 2], @m.middles([1, 1, 5], [6, 2, 1])
+		assert_equal [3, 20], @m.middles([5, 3, 5], [1, 20, 11])
+		assert_equal [3, 20], @m.middles([5, 3, 0], [1, 20, 11])
+		
+	end
 end
