717b0baf4a3d8d20a87497dc3c65750971ed5cc9
Alec Lamson <lamsonak@s.dcsdk12.org>
2015-09-20 14:43:10 -0600

    arrays

--- arrays.rb
+++ arrays.rb
@@ -4,8 +4,24 @@ module Arrays
 	end
 	
 	def max array
-		array.index
+		array.max do |element|
+  		element.field
+	end
+	end
+	def lucky13? array
+		if [array !1 && !7]
+			lucky13 = true
+		else false
+		end
+	end
+	def same_ends? array
+		if [array[0] == array.last]
+			n = true
+		else n = false
+		end
+	end
+	def ten_run 
+		
 		
 	end
-	
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -17,9 +17,22 @@ class ArraysTest < MiniTest::Test
 	end
 
 	def test_max
-		assert_equal 5, @max([1, 2, 3, 4, 5])
-		assert_equal 5, @max([1, 2, 5, 4, 2])
-		assert_equal 9, @max([1, 2, 6, 4, 5, 9, 2])
-		assert_equal 7, @max([1, 2, 7, 4, 5, 2])
+		assert_equal 5, @m.max([1, 2, 3, 4, 5])
+		assert_equal 5, @m.max([1, 2, 5, 4, 2])
+		assert_equal 9, @m.max([1, 2, 6, 4, 5, 9, 2])
+		assert_equal 7, @m.max([1, 2, 7, 4, 5, 2])
 	end
+	def test_lucky13
+		assert_equal true, @m.lucky13([2, 5, 7, 9])
+		assert_equal false, @m.lucky13([2, 5, 7, 3])
+		assert_equal false, @m.lucky13([1, 5, 7, 3])
+		assert_equal false, @m.lucky13([1, 5, 7, 10])
+		
+	end
+	def test_same_ends
+		assert_equal true, @m.same_ends([3, 4, 6, 3])
+		assert_equal false, @m.same_ends([3, 5, 3, 5, 5])
+	end
+	
 end
+
